1cbcfc358487b887bc7d7b0fb8471df6f7c3d775
Chun-Hui Gao <gaospecial@gmail.com>
2021-03-29 16:48:54 +0800

    add KEGG organisms ecs, msm, mtu to internal db

--- cache_KEGG_db.R
+++ cache_KEGG_db.R
@@ -1,4 +1,4 @@
 # create local KEGG db
 library(createKEGGdb)
-createKEGGdb::create_kegg_db(species = c("eco","ppu"))
+createKEGGdb::create_kegg_db(species = c("eco","ppu","ecs","msm","mtu"))
 install.packages("KEGG.db_1.0.tar.gz", type = "source")
